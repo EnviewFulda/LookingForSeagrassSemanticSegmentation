89515fd6dd415528ae26294f027e4d9906f724cd
Aequalitas <aequalitas@outlook.de>
2019-04-25 14:26:25 +0200

    Update deeplabV3plusSS.py

--- nets/deeplabV3plusSS.py
+++ nets/deeplabV3plusSS.py
@@ -2,27 +2,15 @@ import tensorflow as tf
 import nnUtils as util
 from nets.deeplabV3Plus import model, common
 
-STRIDE = 2
-K = 3
-initDepth = 128
-outputStride = 8 # or 16
-backBone = "resnet_v2_50" # or "resnet_v2_101"
-batchNormDecay = 0.9997
-preTrainedModel = None
-
 def net(image, classes, MODE):    
-    #encoding - convolution/pooling
     # Note: arg_scope is optional for inference.
-    print("INPUTSHAPE: ", image.get_shape())
-
-    
    # deeplabv3plus with xception
     
     model_options = common.ModelOptions(
       outputs_to_num_classes={"semantic":classes},
       crop_size=[256,512],
       atrous_rates=[12,24,36], #  with OS of 16 -> [6,12,18]
-      output_stride=outputStride)
+      output_stride= 8) # or 16)
     
     L = model.multi_scale_logits(
       image,
