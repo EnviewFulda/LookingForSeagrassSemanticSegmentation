ea12a55334d7a0372e835601173f96f132edb25e
Aequalitas <aequalitas@outlook.de>
2019-04-25 14:27:36 +0200

    Update nnUtils.py

--- nnUtils.py
+++ nnUtils.py
@@ -21,7 +21,7 @@ def conv(input, filter, name, pad="SAME", dilation=0, dropR=0.3):
     #conv_bias = tf.nn.bias_add(conv, bias_variable([filter[3]], name=name+"b1"))
     batch_norm = tf.contrib.layers.batch_norm(conv)
     
-    relu = tf.nn.relu6(batch_norm)
+    relu = tf.nn.relu(batch_norm)
     
     #if leakyR:
     #relu = tf.nn.leaky_relu(batch_norm,alpha=0.2,name=None)
@@ -66,17 +66,17 @@ def deconv_filter(shape, name):
            initializer=tf.contrib.layers.xavier_initializer())
 
 def deconv(layer, outputShape, filterShape, name, stride=2):
-    #filter = deconv_filter(filterShape, "deconvF"+layer.name[:len(layer.name)-2])
+    filter = deconv_filter(filterShape, "deconvF"+layer.name[:len(layer.name)-2])
 
-    #deconv = tf.nn.conv2d_transpose(
-    #        layer,
-    #        filter,
-    #        outputShape,
-    #        strides=[1,stride,stride,1],
-    #        padding="SAME",
-    #        name=name
-    #    )
+    deconv = tf.nn.conv2d_transpose(
+            layer,
+            filter,
+            outputShape,
+            strides=[1,stride,stride,1],
+            padding="SAME",
+            name=name
+        )
     
-    deconv = tf.image.resize_bilinear(
-                  layer, outputShape, align_corners=True)
+    #deconv = tf.image.resize_bilinear(
+     #             layer, outputShape, align_corners=True)
     return deconv
